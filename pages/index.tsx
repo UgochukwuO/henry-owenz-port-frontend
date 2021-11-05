fd0a5dba2575edf87af9cf6be87fe7b3c451c5d7
UgochukwuO <ugo.onwuzurike@yahoo.com>
2021-11-05 13:27:16 -0500

    First Commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import styles2 from '../styles/blog.module.css'
 import { gql } from "@apollo/client";
 import client from "../apollo-client";
 
